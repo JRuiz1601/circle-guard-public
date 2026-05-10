ea82c264cfad38652fe7d6f386d0948d0d79db62
Juan Esteban Ruiz Ome <1110366598@u.icesi.edu.co>
2026-05-10 16:08:28 -0500

    fix(tests): add H2 for form tests and valid JWT secret for identity

--- services/circleguard-form-service/src/test/java/com/circleguard/form/controller/AttachmentControllerTest.java
+++ services/circleguard-form-service/src/test/java/com/circleguard/form/controller/AttachmentControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -13,6 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class AttachmentControllerTest {
 
     @Autowired
