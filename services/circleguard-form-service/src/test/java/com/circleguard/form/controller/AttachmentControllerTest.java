95a060e3349710a8a874537225baa4eb9f54a7d3
Juan Esteban Ruiz Ome <1110366598@u.icesi.edu.co>
2026-05-10 18:00:40 -0500

    fix: mock KafkaTemplate in all form-service integration tests to fix missing bean

--- services/circleguard-form-service/src/test/java/com/circleguard/form/controller/AttachmentControllerTest.java
+++ services/circleguard-form-service/src/test/java/com/circleguard/form/controller/AttachmentControllerTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,6 +24,9 @@ class AttachmentControllerTest {
     
     @MockBean
     private StorageService storageService;
+
+    @MockBean
+    private KafkaTemplate<String, Object> kafkaTemplate;
 
     @Test
     void shouldUploadFile() throws Exception {
--- services/circleguard-form-service/src/test/java/com/circleguard/form/controller/FormIntegrationTests.java
+++ services/circleguard-form-service/src/test/java/com/circleguard/form/controller/FormIntegrationTests.java
@@ -3,7 +3,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -17,6 +19,9 @@ class FormIntegrationTests {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
     @Test
     void getQuestionnairesReturnsOk() throws Exception {
         mockMvc.perform(get("/api/v1/questionnaires"))
@@ -24,7 +29,7 @@ class FormIntegrationTests {
     }
 
     @Test
-    void postQuestionnaireWithoutBodyReturnsBadRequestOrOk() throws Exception {
+    void postQuestionnaireWithValidBodyReturnsOk() throws Exception {
         mockMvc.perform(post("/api/v1/questionnaires")
             .contentType(MediaType.APPLICATION_JSON)
             .content("{\"title\":\"Test\",\"description\":\"Desc\",\"active\":false}"))
